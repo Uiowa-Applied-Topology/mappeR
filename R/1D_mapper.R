6e10f8a996fdaa423eb8b6a800f9116e380bbf96
George CK <georgeck612@gmail.com>
2024-01-13 18:04:33 -0600

    initial commit. basic 1D mapper function

---
+++ R/1D_mapper.R
@@ -0,0 +1,126 @@
+# given an interval, calculates endpoints of a fixed number of evenly spaced, equal length, overlapping subintervals with a fixed percent overlap.
+get_width_balanced_endpoints <- function(min_val, max_val, num_bins, percent_overlap) {
+
+  even_length = (max_val - min_val)/num_bins # widths with zero percent overlap
+  nudge = (percent_overlap/100)*even_length
+
+  left_ends = c(min_val) # first left endpoint is unchanged
+  right_ends = c(min_val + even_length) # first right endpoint is unchanged
+
+  for (j in 1:(num_bins-1)) {
+    leftcut = min_val + j*(even_length - nudge) # move the left endpoint so it overlaps with its neighbor appropriately
+    rightcut = leftcut + even_length # don't change the length of the segment yet
+    left_ends = append(left_ends, leftcut)
+    right_ends = append(right_ends, rightcut)
+  }
+
+  scale_factor = (max_val - min_val)/(right_ends[num_bins] - min_val) # scale by pretending min_val = 0
+
+  bin_ends = cbind(left_ends, right_ends) # make bins
+
+  bin_ends = scale_factor*(bin_ends - min_val) + min_val # scale the bins after translating, then translate back
+
+  return(bin_ends)
+}
+
+# makes a list of subsets of the input data, according to specified "bins."
+make_bins <- function(data, filtered_data, bin_ends) {
+  bins = list()
+  num_bins = nrow(bin_ends)
+
+  for (i in 1:num_bins) {
+    bin_assignments = c()
+    bin_left = as.numeric(bin_ends[i,1])
+    bin_right = as.numeric(bin_ends[i,2])
+    for (j in 1:length(filtered_data)) { # loop through filtered data so we can look at level sets
+      if ((bin_left - filtered_data[j] <= 0) & (bin_right - filtered_data[j] >= 0)) {
+        bin_assignments = append(bin_assignments, j) # don't need the whole datapoint, just the index
+      }
+    }
+    if (length(bin_assignments) != 0) { # this means our bin is empty
+      bins[[i]] = data[bin_assignments,] # get a subset of the original data based on the indices we collected
+    } else {
+      bins[[i]] = list() # bin still exists, it's just empty
+    }
+  }
+  return(bins)
+}
+
+# performs single linkage clustering and outputs clusters based on a rough heuristic.
+get_single_linkage_clusters <- function(dists) {
+  hcl = hclust(dists, method = "single")
+
+  cutval = 0
+  maxdiff = 0
+  heights = sort(unique(cophenetic(hcl))) # merge heights of dendrogram
+
+  # idea is the most persistent number of clusters is the best one
+  # this number is the tallest section of the dendrogram without merges
+  for (i in 1:(length(heights)-1)){
+    currentdiff = abs(heights[i]-heights[i+1])
+    if (currentdiff > maxdiff) {
+      maxdiff = currentdiff
+      cutval = heights[i]
+    }
+  }
+
+  # if the dendrogram is "very short" we might expect the best number of clusters is one
+  if (max(heights) < 0.1) { # I made this number up
+    return(cutree(hcl, k=1))
+  } else {
+    return(cutree(hcl, h=cutval))
+  }
+}
+
+# given binned data and the full data's distance matrix, clusters within each bin. keeps track of total clusters across bins.
+get_clusters <- function(bins, dists, method) {
+  clustered_data = list()
+  cluster_count = 0
+
+  for (i in 1:length(bins)){
+    d = dist_subset(dists, rownames(bins[[i]])) # assuming your data has identifiers!!
+    clust = switch(tolower(method), "single" = get_single_linkage_clusters(d), stop("please tell george to do more clustering methods"))
+    clustered_data = append(clustered_data, clust + cluster_count)
+    cluster_count = cluster_count + max(clust) # update the total
+  }
+  return(append(clustered_data, cluster_count))
+}
+
+# constructs an abstract graph with clusters as vertices and nonempty intersections between clusters as edges.
+construct_graph <- function(clustered_data) {
+  num_vertices = as.numeric(clustered_data[[length(clustered_data)]])
+  amat = matrix(, nrow = num_vertices, ncol = num_vertices)
+  for (i in 1:(num_vertices-1)) {
+    for (j in i:num_vertices) {
+      if (i == j) {
+        amat[i, j] = 0
+      } else {
+        my_cluster = clustered_data[clustered_data == i] # get the datapoints in the ith cluster
+        compare_cluster = clustered_data[clustered_data == j] # get the datapoints in the jth cluster
+        if (length((intersect(names(my_cluster), names(compare_cluster)))) != 0) {
+          amat[i, j] = 1
+        } else {
+          amat[i, j] = 0
+        }
+      }
+    }
+  }
+  return(amat)
+}
+
+# runner function for 1D mapper; outputs an adjacency matrix
+mapper <- function(data, filtered_data, dists, num_bins, percent_overlap, clustering_method) {
+  # bin data according to filter values
+  binned_data = make_bins(data, filtered_data, get_width_balanced_endpoints(min(filtered_data), max(filtered_data), num_bins, percent_overlap))
+
+  # cluster data
+  clustered_data = get_clusters(binned_data, dists, clustering_method)
+
+  # construct and plot mapper graph
+  amat = construct_graph(clustered_data)
+  return(graph_from_adjacency_matrix(amat, mode="max"))
+}
+
+
+
+
---
+++ tests/testthat/test-binning.R
@@ -0,0 +1,47 @@
+test_that("interval math works", {
+  vars = c(sort(c(sample(1:2, 1)*(-1)*runif(1), sample(1:2, 1)*(-1)*runif(1))), runif(2))
+  left_end  = vars[1]
+  right_end = vars[2]
+  num_bins = floor(vars[3]*100)
+  percent_overlap = vars[4]*100
+
+  bins = get_width_balanced_endpoints(left_end, right_end, num_bins, percent_overlap)
+
+  expect_equal(nrow(bins), num_bins) # output should contain correct number of bins
+  expect_equal(as.numeric(bins[1,1]), left_end) # leftmost endpoint should be correct
+  expect_equal(as.numeric(bins[num_bins, 2]), right_end) # rightmost endpoint should be correct
+
+  bin_length = as.numeric(abs(bins[1,1] - bins[1,2]))
+
+  for (i in 2:(num_bins - 1)) {
+    expect_equal(as.numeric(abs(bins[i, 1] - bins[i, 2])), bin_length) # bin lengths should be consistent
+
+    left_overlap_percent = as.numeric(100*abs(bins[i, 1] - bins[i-1, 2])/bin_length)
+    right_overlap_percent = as.numeric(100*abs(bins[i, 2] - bins[i+1, 1])/bin_length)
+
+    expect_equal(left_overlap_percent, percent_overlap) # overlap on the left is correct
+    expect_equal(right_overlap_percent, percent_overlap) # overlap on the right is correct
+  }
+
+  expect_equal(as.numeric(100*abs(bins[1, 2] - bins[2, 1])/bin_length), percent_overlap) # first bin overlap is correct
+  expect_equal(as.numeric(100*abs(bins[nrow(bins), 1] - bins[nrow(bins)-1, 2])/bin_length), percent_overlap) # last bin overlap is correct
+})
+
+# idea from https://stackoverflow.com/questions/32345484/does-vector-exist-in-matrix
+test_that("data binning works", {
+  random_left_ends = runif(20)*100
+  bin_ends = cbind(random_left_ends, random_left_ends + runif(20)*100)
+  data = data.frame(x = runif(50)*100, y= runif(50)*100)
+  bins = make_bins(data, data$x, bin_ends)
+  for (data_idx in 1:50) {
+    for (bin_idx in 1:20) {
+      if ((data$x[data_idx] >= bin_ends[bin_idx, 1]) & (data$x[data_idx] <= bin_ends[bin_idx, 2])) {
+        expect_true(any(bins[[bin_idx]] == data[data_idx,][col(bins[[bin_idx]])])) # datapoint should correctly be in appropriate bin
+      } else {
+        if(length(bins[[bin_idx]]) != 0) {
+          expect_false(any(bins[[bin_idx]] == data[data_idx,][col(bins[[bin_idx]])])) # should also correctly not be in inappropriate bin
+        }
+      }
+    }
+  }
+})
