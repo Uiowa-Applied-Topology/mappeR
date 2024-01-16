c956226a5e5dbb105c8a3b43491df69db7627111
George CK <georgeck612@gmail.com>
2024-01-16 13:16:36 -0600

    fix issue when only one datapoint is in a bin

--- R/1D_mapper.R
+++ R/1D_mapper.R
@@ -54,13 +54,17 @@ get_single_linkage_clusters <- function(dists) {
   maxdiff = 0
   heights = sort(unique(cophenetic(hcl))) # merge heights of dendrogram
 
-  # idea is the most persistent number of clusters is the best one
-  # this number is the tallest section of the dendrogram without merges
-  for (i in 1:(length(heights)-1)){
-    currentdiff = abs(heights[i]-heights[i+1])
-    if (currentdiff > maxdiff) {
-      maxdiff = currentdiff
-      cutval = heights[i]
+  if (length(heights) == 1) {
+    cutval = heights[1]
+  } else {
+    # idea is the most persistent number of clusters is the best one
+    # this number is the tallest section of the dendrogram without merges
+    for (i in 1:(length(heights)-1)){
+      currentdiff = abs(heights[i]-heights[i+1])
+      if (currentdiff > maxdiff) {
+        maxdiff = currentdiff
+        cutval = heights[i]
+      }
     }
   }
 
@@ -78,6 +82,11 @@ get_clusters <- function(bins, dists, method) {
   cluster_count = 0
 
   for (i in 1:length(bins)){
+    if (nrow(bins[[i]]) == 1) {
+      clustered_data[[i]] = setNames(1, rownames(bins[[i]])) + cluster_count
+      cluster_count = cluster_count + 1
+      next
+    }
     d = dist_subset(dists, rownames(bins[[i]])) # assuming your data has identifiers! also I am not sure of this function's behavior on non-dissimilarity matrices.
     clust = switch(tolower(method), "single" = get_single_linkage_clusters(d), stop("please tell george to do more clustering methods"))
     clustered_data[[i]] = clust + cluster_count
@@ -123,12 +132,15 @@ construct_graph <- function(clustered_data) {
 # runner function for 1D mapper; outputs bins, clusters, and the mapper graph.
 get_mapper_data <- function(data, filtered_data, dists, num_bins, percent_overlap, clustering_method) {
   # bin data according to filter values
+  print("binning...")
   binned_data = make_bins(data, filtered_data, get_width_balanced_endpoints(min(filtered_data), max(filtered_data), num_bins, percent_overlap))
 
   # cluster data
+  print("clustering...")
   clustered_data = get_clusters(binned_data, dists, clustering_method)
 
   # construct mapper graph
+  print("making mapper graph...")
   graph_data = construct_graph(clustered_data)
   amat = graph_data[[1]]
   edge_overlaps = graph_data[[2]]
@@ -172,8 +184,11 @@ visualize_mapper_data <- function(mapper_data) {
   }
   size_vector = (size_vector/sqrt(sum(size_vector^2)))*200
   edge_weights = (edge_weights/sqrt(sum(edge_weights^2)))*25
+  print("coloring nodes...")
   setNodeColorMapping("bin", c(1, num_bins/2, num_bins), c("#998ec3", "#f7f7f7", "#f1a340"))
+  print("resizing nodes...")
   setNodeSizeMapping("cluster", 1:num_vertices, sizes = size_vector)
+  print("resizing edge widths...")
   setEdgeLineWidthMapping("overlap", 1:num_edges, widths = edge_weights)
 }
 
