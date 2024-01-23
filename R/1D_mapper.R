2ae4cad390fbe7a1c1697b6316d52114dbb02dc2
georgeck612 <georgeck612@gmail.com>
2024-01-22 23:36:09 -0600

    segregate visualization function

--- R/1D_mapper.R
+++ R/1D_mapper.R
@@ -163,19 +163,7 @@ get_bin_vector <- function(clustered_data) {
   return(clusters_and_bins)
 }
 
-visualize_mapper_data <- function(mapper_data) {
-  clustered_data = mapper_data[[1]]
-  mapper_graph = mapper_data[[2]]
-  edge_weights = mapper_data[[3]]
-
-  num_vertices = gorder(mapper_graph)
-  num_edges = gsize(mapper_graph)
-  num_bins = length(clustered_data)
-  bin_vector = get_bin_vector(clustered_data)
-  cygraph = set_vertex_attr(mapper_graph, "bin", value = bin_vector)
-  cygraph = set_vertex_attr(cygraph, "cluster", value = 1:num_vertices)
-  cygraph = set_edge_attr(cygraph, "overlap", value = (edge_weights/sqrt(sum(edge_weights^2)))*25)
-
+get_size_vector <- function(clustered_data) {
   size_vector = c()
 
   flattened_data = unlist(clustered_data)
@@ -187,7 +175,36 @@ visualize_mapper_data <- function(mapper_data) {
 
   size_vector = (size_vector/sqrt(sum(size_vector^2)))*200
 
-  cygraph = set_vertex_attr(cygraph, "cluster_size", value = size_vector)
+  return(size_vector)
+}
+
+get_color_vector <- function(bin_vector) {
+  colfun = colorRampPalette(c('#998ec3', '#f7f7f7', '#f1a340'))
+  colors = colfun(num_bins)
+  color_vector = c()
+
+  for (i in 1:num_vertices) {
+    color_vector = append(color_vector, colors[bin_vector[i]])
+  }
+
+  return(color_vector)
+}
+
+visualize_mapper_data <- function(mapper_data) {
+  clustered_data = mapper_data[[1]]
+  mapper_graph = mapper_data[[2]]
+  edge_weights = mapper_data[[3]]
+
+  num_vertices = gorder(mapper_graph)
+  num_edges = gsize(mapper_graph)
+  num_bins = length(clustered_data)
+  bin_vector = get_bin_vector(clustered_data)
+
+  cygraph = set_vertex_attr(mapper_graph, "bin", value = bin_vector)
+  cygraph = set_vertex_attr(cygraph, "cluster", value = 1:num_vertices)
+  cygraph = set_edge_attr(cygraph, "overlap", value = (edge_weights/sqrt(sum(edge_weights^2)))*25)
+  cygraph = set_vertex_attr(cygraph, "cluster_size", value = get_size_vector(clustered_data))
+  cygraph = set_vertex_attr(cygraph, "color", value = get_color_vector(bin_vector))
 
   createNetworkFromIgraph(cygraph)
 
@@ -197,19 +214,13 @@ visualize_mapper_data <- function(mapper_data) {
 
   nodeLabels <- mapVisualProperty('node label', 'cluster', 'p')
   nodeSizes <- mapVisualProperty('node size', 'cluster_size', 'p')
-  nodeFills <- mapVisualProperty('node fill color', 'bin', 'c', c(1, (num_bins-1)/2, num_bins), c('#998ec3', '#f7f7f7', '#f1a340')) # TODO: fix for odd numbers of bins
   edgeWidth <- mapVisualProperty('edge width', 'overlap', 'p')
 
-  createVisualStyle(style.name, defaults, list(nodeLabels, nodeSizes, nodeFills, edgeWidth))
+  createVisualStyle(style.name, defaults, list(nodeLabels, nodeSizes, edgeWidth))
 
   setVisualStyle(style.name)
 
-  # setNodeColorMapping("bin", c(1, num_bins/2, num_bins), c("#998ec3", "#f7f7f7", "#f1a340"))
-  #
-  # if (length(edge_weights) != 0) {
-  #     setEdgeLineWidthMapping("overlap", 1:num_edges, widths = edge_weights)
-  # }
-
+  setNodeColorMapping("bin", c(1, num_bins/2, num_bins), c("#998ec3", "#f7f7f7", "#f1a340"), style.name = style.name)
 }
 
 cymapper <- function(data, filtered_data, dists, num_bins, percent_overlap, clustering_method) {
