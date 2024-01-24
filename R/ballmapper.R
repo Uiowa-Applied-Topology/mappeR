522bb2422a21da70aa1552e84dc5bc60f8c836ba
georgeck612 <georgeck612@gmail.com>
2024-01-24 01:58:12 -0600

    add basic ball binning (...untested)

--- R/ballmapper.R
+++ R/ballmapper.R
@@ -1,11 +1,22 @@
-create_cover_vector <- function(data, dists, eps) {
-  cover_vector = list()
+create_bins <- function(data, dists, eps) {
+  dists = as.matrix(dists) # because I am stupid and usedists isn't working
+  balls = list()
   marked = rep(FALSE, nrow(data))
-  while(FALSE %in% marked) { # while points are still uncovered
-    ballbin = list()
-    uncovered_idxs = which(marked)
-    random_point = sample(data[uncovered_idxs], 1)$name
+  names(marked) = rownames(dists)
+  while (FALSE %in% marked) {
+    current_ball_center = NULL
+    if (length(which(marked)) == 0) {
+      current_ball_center = sample(rownames(data), 1) # pick a random point if no points are marked
 
-
+    } else {
+      datanames = rownames(data)
+      unmarked_points = datanames[which(!marked)]
+      current_ball_center = sample(unmarked_points, 1)
+    }
+    all_dists = dists[current_ball_center,] # all distances away from ball center
+    balled_dists = all_dists[all_dists < eps] # now restrict to in the ball
+    marked[names(balled_dists)] = TRUE # mark points inside the ball as covered
+    balls = append(balls, list(balled_dists)) # add to our bin list
   }
+  return(balls)
 }
