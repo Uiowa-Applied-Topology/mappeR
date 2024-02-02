44f9270c0b158d5000866b8cc2179508f8b39985
georgeck612 <georgeck612@gmail.com>
2024-02-02 00:23:18 -0600

    segregate ballmapper math from viz

--- R/ballmapper.R
+++ R/ballmapper.R
@@ -1,5 +1,4 @@
 # greedy epsilon net algorithm from Dłotko
-# outputs a list of
 create_balls <- function(data, dists, eps) {
   dists = as.matrix(dists) # because I am stupid and usedists isn't working we use a symmetric matrix
   balls = list()
@@ -58,53 +57,4 @@ get_ballmapper_data <- function(data, dists, eps) {
   mapper_graph = graph_from_adjacency_matrix(amat, mode="max") # so we only have to record the upper half
 
   return(list(formatted_balled_data, mapper_graph, edge_overlaps))
-}
-
-get_size_vector <- function(clustered_data, num_vertices) {
-  size_vector = c()
-
-  flattened_data = unlist(clustered_data)
-
-  for (i in 1:num_vertices) {
-    my_cluster = flattened_data[flattened_data == i]
-    size_vector = append(size_vector, length(my_cluster))
-  }
-
-  size_vector = (size_vector/sqrt(sum(size_vector^2)))*200
-
-  return(size_vector)
-}
-
-visualize_ballmapper_data <- function(mapper_data) {
-  clustered_data = mapper_data[[1]]
-  mapper_graph = mapper_data[[2]]
-  edge_weights = mapper_data[[3]]
-
-  num_vertices = gorder(mapper_graph)
-  num_edges = gsize(mapper_graph)
-  num_bins = length(clustered_data)
-
-  cygraph = set_vertex_attr(mapper_graph, "cluster", value = 1:num_vertices)
-  cygraph = set_edge_attr(cygraph, "overlap", value = (edge_weights/sqrt(sum(edge_weights^2)))*25)
-  cygraph = set_vertex_attr(cygraph, "cluster_size", value = get_size_vector(clustered_data, num_vertices))
-
-  createNetworkFromIgraph(cygraph)
-
-  style.name = "mapperstyle"
-  defaults <- list(NODE_SHAPE = "ellipse",
-                   EDGE_TRANSPARENCY = 120)
-
-  nodeLabels <- mapVisualProperty('node label', 'cluster', 'p')
-  nodeSizes <- mapVisualProperty('node size', 'cluster_size', 'p')
-  edgeWidth <- mapVisualProperty('edge width', 'overlap', 'p')
-
-  createVisualStyle(style.name, defaults, list(nodeLabels, nodeSizes, edgeWidth))
-
-  setVisualStyle(style.name)
-
-}
-
-cyballmapper <- function(data, dists, eps) {
-  visualize_ballmapper_data(get_ballmapper_data(data, dists, eps))
-  return(invisible(NULL))
 }
