b5584014605684f616e6c98a26e0325d5aa9d324
georgeck612 <georgeck612@gmail.com>
2024-02-02 01:41:24 -0600

    add node borders

--- R/1D_mapper_viz.R
+++ R/1D_mapper_viz.R
@@ -1,60 +1,59 @@
-get_bin_vector <- function(clustered_data) {
+source("R/1D_mapper.R")
+
+# categorizes clusters by bin. returns a vector of bin numbers corresponding to each cluster.
+get_bin_vector <- function(binclust_data) {
   clusters_and_bins = c()
-  for (i in 1:length(clustered_data)) {
-    for (j in unique(clustered_data[[i]])) {
-      clusters_and_bins = append(clusters_and_bins, i)
+  for (i in 1:length(binclust_data)) { # go through each bin
+    for (j in unique(binclust_data[[i]])) { # the bin contains clusters
+      clusters_and_bins = append(clusters_and_bins, i) # tag each cluster with the bin number
     }
   }
   return(clusters_and_bins)
 }
 
-get_size_vector <- function(clustered_data, num_vertices) {
+# calculates how large nodes should be based on how many datapoints it contains
+# returns a vector of cluster sizes, normalized and rescaled
+get_size_vector <- function(binclust_data, num_vertices) {
   size_vector = c()
 
-  flattened_data = unlist(clustered_data)
+  flattened_data = unlist(binclust_data) # we don't care about bins here
 
+  # there is some lapply way to do this but i actually don't know how to code
   for (i in 1:num_vertices) {
-    my_cluster = flattened_data[flattened_data == i]
-    size_vector = append(size_vector, length(my_cluster))
+    my_cluster = flattened_data[flattened_data == i] # find all the points with the same cluster number
+    size_vector = append(size_vector, length(my_cluster)) # record that cluster's length
   }
 
-  size_vector = (size_vector/sqrt(sum(size_vector^2)))*200
+  scale_factor = 200 # change according to your eyeballs/monitor
+
+  size_vector = (size_vector/sqrt(sum(size_vector^2)))*scale_factor
 
   return(size_vector)
 }
 
-get_color_vector <- function(bin_vector, num_vertices, num_bins) {
-  colfun = colorRampPalette(c('#998ec3', '#f7f7f7', '#f1a340'))
-  colors = colfun(num_bins)
-  color_vector = c()
-
-  for (i in 1:num_vertices) {
-    color_vector = append(color_vector, colors[bin_vector[i]])
-  }
-
-  return(color_vector)
-}
-
 visualize_mapper_data <- function(mapper_data) {
-  clustered_data = mapper_data[[1]]
+  # get all of our data
+  binclust_data = mapper_data[[1]]
   mapper_graph = mapper_data[[2]]
   edge_weights = mapper_data[[3]]
 
   num_vertices = gorder(mapper_graph)
   num_edges = gsize(mapper_graph)
-  num_bins = length(clustered_data)
-  bin_vector = get_bin_vector(clustered_data)
+  num_bins = length(binclust_data)
+  bin_vector = get_bin_vector(binclust_data)
 
   cygraph = set_vertex_attr(mapper_graph, "bin", value = bin_vector)
   cygraph = set_vertex_attr(cygraph, "cluster", value = 1:num_vertices)
   cygraph = set_edge_attr(cygraph, "overlap", value = (edge_weights/sqrt(sum(edge_weights^2)))*25)
-  cygraph = set_vertex_attr(cygraph, "cluster_size", value = get_size_vector(clustered_data, num_vertices))
-  cygraph = set_vertex_attr(cygraph, "color", value = get_color_vector(bin_vector, num_vertices, num_bins))
+  cygraph = set_vertex_attr(cygraph, "cluster_size", value = get_size_vector(binclust_data, num_vertices))
 
   createNetworkFromIgraph(cygraph)
 
   style.name = "mapperstyle"
   defaults <- list(NODE_SHAPE = "ellipse",
+                   NODE_FILL_COLOR = "#FFFFFF",
+                   NODE_BORDER_WIDTH = 100,
+                   NODE_BORDER_TRANSPARENCY = 0,
                    EDGE_TRANSPARENCY = 120)
 
   nodeLabels <- mapVisualProperty('node label', 'cluster', 'p')
@@ -65,7 +64,10 @@ visualize_mapper_data <- function(mapper_data) {
 
   setVisualStyle(style.name)
 
-  setNodeColorMapping("bin", c(1, num_bins/2, num_bins), c("#998ec3", "#f7f7f7", "#f1a340"), style.name = style.name)
+
+  setNodeBorderWidthDefault(100, style.name = style.name)
+  setNodeBorderColorMapping("bin", c(1, num_bins/2, num_bins), c("#998ec3", "#f7f7f7", "#f1a340"), style.name = style.name)
+  # setNodeBorderColorMapping("bin", colors = t(get_color_vector(bin_vector, num_vertices, num_bins)))
 }
 
 cymapper <- function(data, filtered_data, dists, num_bins, percent_overlap, clustering_method) {
