ffda9e1f58670e925f143065c2872102b2a18ee6
George CK <georgeck612@gmail.com>
2024-02-13 02:07:34 -0600

    add comments, pass cluster data to cytoscape

--- R/style_tools.R
+++ R/style_tools.R
@@ -1,11 +1,12 @@
-get_size_vector <- function(binclust_data, num_vertices) {
+# given our mappered data, returns the a vector with the number of datapoints per cluster.
+get_cluster_sizes <- function(binclust_data, num_vertices) {
   size_vector = c()
 
-  flattened_data = unlist(binclust_data)
+  flattened_data = unlist(binclust_data) # bins are not important here
 
   for (i in 1:num_vertices) {
-    my_cluster = flattened_data[flattened_data == i]
-    size_vector = append(size_vector, length(my_cluster))
+    my_cluster = flattened_data[flattened_data == i] # get just the data in this cluster
+    size_vector = append(size_vector, length(my_cluster)) # record the number of datapoints
   }
 
   return(size_vector)
@@ -22,48 +23,64 @@ get_bin_vector <- function(binclust_data) {
   return(clusters_and_bins)
 }
 
+# computes a measure of cluster "tightness" as a number between 0 and 1, based on the medoids of the clusters.
 get_cluster_tightness_vector <- function(dists, binclust_data, num_vertices) {
   flattened_data = unlist(binclust_data) # we don't care about bins here
   res = c()
 
   # there is some lapply way to do this but i actually don't know how to code
-  for (i in 1:num_vertices) {
+  for (i in 1:num_vertices) { # go cluster by cluster
     min = Inf
     min_name = NULL
-    my_cluster = flattened_data[flattened_data == i] # find all the points with the same cluster number
+    my_cluster = flattened_data[flattened_data == i] # gather points in this cluster
     if (length(my_cluster) == 1) { # trivially tight cluster
       res = append(res, 1)
     } else {
-      # find the medoid of the dataset
-      for (datapoint in names(my_cluster)) {
+      for (datapoint in names(my_cluster)) { # go through all the data in the cluster
         these_dists = dists[datapoint,names(my_cluster)]
-        dist_sum = sum(these_dists)
-        if (dist_sum < min) {
+        dist_sum = sum(these_dists) # sum the distances from this datapoint to all others in the cluster
+        if (dist_sum < min) { # we are looking for the smallest such sum
           min = dist_sum
           min_name = datapoint
         }
       }
-      close_datapoint_dists = dists[min_name,names(my_cluster)]
-      n = length(close_datapoint_dists)
-      min_dist = min(close_datapoint_dists[close_datapoint_dists > 0])
-      dist_sums = sum(close_datapoint_dists)
-      closeness_factor = (length(my_cluster)-1)*min_dist/dist_sums
+
+      close_datapoint_dists = dists[min_name,names(my_cluster)] # distance set of the medoid
+      min_dist = min(close_datapoint_dists[close_datapoint_dists > 0]) # find the closest datapoint to the medoid (assuming we are living in at least a semimetric)
+      dist_sums = sum(close_datapoint_dists) # minimum sum
+      closeness_factor = (length(my_cluster)-1)*min_dist/dist_sums # "normalized" so a score of 1 means the medoid is as close as possible to all the data in the cluster
       res = append(res, closeness_factor)
     }
   }
   return(res)
 }
 
+# calcuates edge weights. for each edge, its weight is the maximum of the relative percent overlaps between the clusters.
 get_edge_weights <- function(overlap_lengths, cluster_sizes, edges) {
   edge_weights = c()
   for (i in 1:length(overlap_lengths)) {
-    head_id = edges[i, 1]
-    tail_id = edges[i, 2]
-    node_size_1 = cluster_sizes[head_id]
-    node_size_2 = cluster_sizes[tail_id]
+    cluster1_id = edges[i, 1]
+    cluster2_id = edges[i, 2]
+    cluster1_size = cluster_sizes[cluster1_id]
+    cluster2_size = cluster_sizes[cluster2_id]
     overlap = overlap_lengths[i]
-    overlap_weight = max(overlap/node_size_1, overlap/node_size_2)
+
+    overlap_weight = max(overlap/cluster1_size, overlap/cluster2_size) # pick the bigger relative overlap
     edge_weights = append(edge_weights, overlap_weight)
   }
   return(edge_weights)
+}
+
+# gives which data is in each cluster
+get_clustered_data <- function(binclust_data, num_vertices) {
+  res = array()
+
+  flattened_data = unlist(binclust_data) # bins are not important here
+
+  for (i in 1:num_vertices) {
+    my_cluster = flattened_data[flattened_data == i] # gather points in this cluster
+    res = append(res, toString(names(my_cluster)))
+  }
+
+  return(res)
 }
