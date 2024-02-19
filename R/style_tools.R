b120e946f25142e6df4b6115496d4c6de6999b2e
George CK <georgeck612@gmail.com>
2024-02-18 20:40:05 -0600

    lapply the tightness function

--- R/style_tools.R
+++ R/style_tools.R
@@ -16,36 +16,29 @@ get_bin_vector <- function(binclust_data) {
   return(bin_by_clusters)
 }
 
+compute_tightness <- function(dists, cluster) {
+  if (length(cluster) == 0) {
+    return(1)
+  } else {
+    cluster_names = names(cluster)
+    these_dists = dists[cluster_names, cluster_names]
+    sums = apply(these_dists, 1, sum)
+    min_sum = min(sums) # sum of distances of the medoid
+    min_dists = these_dists[which(sums == min_sum),] # distance matrix subset of medoid
+    min_dist = min(min_dists[min_dists > 0]) # distance of closest datapoint to medoid
+    closeness_factor = (length(cluster)-1)*(min(min_dist))/min_sum # "normalized" so a score of 1 means the medoid is as close as possible to all the data in the cluster
+    return(closeness_factor)
+  }
+}
+
 # computes a measure of cluster "tightness" as a number between 0 and 1, based on the medoids of the clusters.
 get_cluster_tightness_vector <- function(dists, binclust_data, num_vertices) {
   flattened_data = unlist(binclust_data) # we don't care about bins here
-  res = c()
 
-  # there is some lapply way to do this but i actually don't know how to code
-  for (i in 1:num_vertices) { # go cluster by cluster
-    min = Inf
-    min_name = NULL
-    my_cluster = flattened_data[flattened_data == i] # gather points in this cluster
-    if (length(my_cluster) == 1) { # trivially tight cluster
-      res = append(res, 1)
-    } else {
-      for (datapoint in names(my_cluster)) { # go through all the data in the cluster
-        these_dists = dists[datapoint,names(my_cluster)]
-        dist_sum = sum(these_dists) # sum the distances from this datapoint to all others in the cluster
-        if (dist_sum < min) { # we are looking for the smallest such sum
-          min = dist_sum
-          min_name = datapoint
-        }
-      }
+  clusters = lapply(1:num_vertices, function(x) flattened_data[flattened_data == x])
+  tightness_vector = sapply(clusters, function(x) compute_tightness(dists, x))
 
-      close_datapoint_dists = dists[min_name,names(my_cluster)] # distance set of the medoid
-      min_dist = min(close_datapoint_dists[close_datapoint_dists > 0]) # find the closest datapoint to the medoid (assuming we are living in at least a semimetric)
-      dist_sums = sum(close_datapoint_dists) # minimum sum
-      closeness_factor = (length(my_cluster)-1)*min_dist/dist_sums # "normalized" so a score of 1 means the medoid is as close as possible to all the data in the cluster
-      res = append(res, closeness_factor)
-    }
-  }
-  return(res)
+  return(tightness_vector)
 }
 
 # calcuates edge weights. for each edge, its weight is the maximum of the relative percent overlaps between the clusters.
