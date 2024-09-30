a44a7420cd82b52719e26b7d5e3a7832134d979f
George CK <georgeck612@gmail.com>
2024-09-29 22:32:43 -0500

    add arboretum docs

--- R/arboretum.R
+++ R/arboretum.R
@@ -1,6 +1,11 @@
 # dendrogram processing ---------------------------------------------------
 
-# get tallest branch of a single dendrogram
+#' Find the tallest branch of a dendrogram
+#'
+#' @param dend A single dendrogram.
+#'
+#' @return The height of the tallest branch (longest time between merge heights) of the input dendrogram.
+#' @export
 get_tallest_branch <- function(dend) {
   heights = sort(unique(cophenetic(dend)))
   if (length(heights) <= 1) {
@@ -10,7 +15,13 @@ get_tallest_branch <- function(dend) {
   return(max(branch_lengths))
 }
 
-# cuts...a dendrogram...
+#' Cut a dendrogram
+#'
+#' @param dend A single dendrogram.
+#' @param threshold A mininum tallest branch value.
+#'
+#' @return A named vector whose names are data point names and whose values are cluster labels. The number of clusters is determined to be 1 if the tallest branch of the dendrogram is less than the threshold, or if the index of dispersion (standard deviation squared divided by mean) of the branch heights is too low. Otherwise, we cut at the longest branch of the dendrogram to determine the number of clusters.
+#' @export
 cut_dendrogram <- function(dend, threshold) {
   # TODO remove all the duplicate code lol
   heights = sort(unique(cophenetic(dend))) # merge heights of dendrogram
@@ -33,6 +44,7 @@ cut_dendrogram <- function(dend, threshold) {
   indexofdispersion = sd(heights) ^ 2 / mean(heights)
   dispersioncondition = indexofdispersion < .015
 
+  # uncomment this to plot the dendrograms that come through here with their stats
   # plot(dend, xlab=paste("index of dispersion: ", round(indexofdispersion, 3), " too low? ", dispersioncondition, ", tallest branch: ", round(tallest_branch_height, 3), ", too short? ", thresholdcondition))
 
   if (thresholdcondition | dispersioncondition) {
@@ -42,7 +54,12 @@ cut_dendrogram <- function(dend, threshold) {
   }
 }
 
-# given a bunch of dendrograms, calculates a threshold a dendrogram's merge heights must clear to represent more than one cluster
+#' Cut many dendrograms
+#'
+#' @param dends A list of dendrograms to be cut.
+#'
+#' @return A list of named vectors (one per dendrogram) whose names are data point names and whose values are cluster labels. This function determines a global minimum threshold based on the longest branches in all the input dendrograms, and uses that as a heuristic to gauge if the best number of clusters is 1, or the value obtained by cutting the longest branch.
+#' @export
 process_dendrograms <- function(dends) {
   tallest_branches = sapply(dends, get_tallest_branch)
   biggest_branch_length = max(tallest_branches)
