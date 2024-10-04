411660f0daa8628ed8b270a23c5aa869ebf8a1d4
George CK <georgeck612@gmail.com>
2024-10-04 00:50:14 -0500

    tweak cytoscape visuals

--- R/art_studio.R
+++ R/art_studio.R
@@ -22,15 +22,15 @@ visualize_mapper_data <- function(mapper_data, is_ballmapper = TRUE) {
                                  'd',
                                  1:nrow(nodes),
                                  100 * sqrt(nodes$cluster_size / max(nodes$cluster_size)))
-  edgeWidth <- mapVisualProperty('edge width', 'weight', 'c', c(0, .5, 1), c(0, 5, 10))
+  edgeWidth <- mapVisualProperty('edge width', 'weight', 'c', c(0, .5, 1), c(0, 10, 20))
 
-  fill_colors = lapply(nodes$tightness, function(x) (rgb(x,x,x)))
+  fill_colors = lapply(nodes$tightness/max(nodes$tightness), function(x) rgb(x, x, x))
 
   nodeFillColors <- mapVisualProperty(
     'node fill color',
-    'tightness',
+    'id',
     'd',
-    round(nodes$tightness, 7), # cytoscape rounds things so we will too
+    1:nrow(nodes),
     fill_colors
   )
 
