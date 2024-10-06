165e1947014e50c4f0bd70a040f44f2e7825e868
George CK <georgeck612@gmail.com>
2024-10-06 02:37:35 -0500

    add eccentricity filter

---
+++ R/viewmaster-hub.R
@@ -0,0 +1,26 @@
+#' Compute eccentricity of data points
+#'
+#' @param data A data frame.
+#' @param dists A distance matrix associated to the input data frame.
+#'
+#' @return A vector of centrality measures of data points. Lower values indicate higher centrality.
+#' @export
+#'
+#' @examples
+#' num_points = 5000
+#'
+#' P.data = data.frame(
+#'   x = sapply(1:num_points, function(x)
+#'     sin(x) * 10) + rnorm(num_points, 0, 0.1),
+#'   y = sapply(1:num_points, function(x)
+#'     cos(x) ^ 2 * sin(x) * 10) + rnorm(num_points, 0, 0.1),
+#'   z = sapply(1:num_points, function(x)
+#'     10 * sin(x) ^ 2 * cos(x)) + rnorm(num_points, 0, 0.1)
+#' )
+#'
+#' P.dist = dist(P.data)
+#' eccentricity = eccentricity_filter(P.dist)
+eccentricity_filter <- function(dists) {
+  dists = as.matrix(dists)
+  apply(dists, 1, sum) / nrow(dists)
+}
