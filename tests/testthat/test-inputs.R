dcb2ebc134e8e9b078b1a19991904c09b3e13cfb
George CK <georgeck612@gmail.com>
2024-10-06 01:00:25 -0500

    add test for dist matrix types

---
+++ tests/testthat/test-inputs.R
@@ -0,0 +1,15 @@
+# generate 1000 random data points between -2 and 2
+data = data.frame(x = runif(1000, -2, 2), drop = FALSE)
+cover = create_width_balanced_cover(min(data$x), max(data$x), 10, 25)
+
+test_that("mapper happens ok with distance matrix as a matrix", {
+  expect_no_warning(create_1D_mapper_object(data, as.matrix(dist(data)), data$x, cover, "single"))
+})
+
+test_that("mapper happens ok with distance matrix as a dist", {
+  expect_no_warning(create_1D_mapper_object(data, dist(data), data$x, cover, "single"))
+})
+
+test_that("1D mapper is ok with no clustering method", {
+  expect_no_warning(create_1D_mapper_object(data, dist(data), data$x, cover))
+})
