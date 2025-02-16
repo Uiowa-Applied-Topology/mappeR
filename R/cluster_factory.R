e640ee60d40bbde28f2924950c8ea0b723bb318a
George <georgeck612@gmail.com>
2025-02-16 00:11:36 -0600

    oxygenate

--- R/cluster_factory.R
+++ R/cluster_factory.R
@@ -10,10 +10,9 @@
 #' This function tells the computer to look away for a second, so the goblins come and cluster your data while it's not watching.
 #'
 #' @param dist_mats A list of distance matrices of each bin that is to be clustered.
-#' @param method A string to pass to `fastcluster` to determine clustering method.
-#' @param local_clustering Whether you want clustering to happen in a global (entire dataset visible) or local (only current level set visible) context. Defaults to `TRUE`.
+#' @param clusterer A function which handles a list of distance matrices.
 #'
-#' @return A list containing named vectors (one per bin), whose names are data point names and whose values are cluster labels (within each bin)
+#' @return The output of `clusterer(dist_mats)`, which needs to be a list containing named vectors (one per bin), whose names are data point names and whose values are cluster labels (within each bin)
 get_raw_clusters <- function(dist_mats, clusterer) {
   return(clusterer(dist_mats))
 }
@@ -36,16 +35,15 @@ subset_dists <- function(bin, dists) {
   }
 }
 
-#' Initate the clustering process
+#' Perform the clustering step in mapper
 #'
 #' This function processes the binned data and global distance matrix to return freshly clustered data.
 #'
 #' @param bins A list containing "bins" of vectors of names of data points.
 #' @param dists A distance matrix containing pairwise distances between named data points.
-#' @param method A string to pass to [hclust] to determine clustering method.
-#' @param local_clustering Whether you want clustering to happen in a global (entire dataset visible) or local (only current level set visible) context. Defaults to `TRUE`.
+#' @param clusterer A function which handles a list of distance matrices.
 #'
-#' @return A list containing named vectors (one per bin), whose names are data point names and whose values are cluster labels
+#' @return The output of `clusterer` applied to a list of distance matrices, which should be a list containing named vectors (one per bin), whose names are data point names and whose values are cluster labels.
 get_clusters <- function(bins, dists, clusterer) {
   # more than one bin, need more than one distance matrix
   if (is.list(bins)) {
@@ -96,10 +94,8 @@ convert_to_clusters <- function(bins) {
 #' Perform hierarchical clustering and process dendrograms
 #'
 #' @param dist_mats A list of distance matrices to be used for clustering.
-#' @param method A string to pass to [hclust] to determine clustering method.
-#' @param local_clustering Whether you want clustering to happen in a global (entire dataset visible) or local (only current level set visible) context. Defaults to `TRUE`.
 #'
-#' @return A list containing named vectors (one per dendrogram), whose names are data point names and whose values are cluster labels
+#' @return A list containing named vectors (one per dendrogram), whose names are data point names and whose values are cluster labels.
 get_hierarchical_clusters <- function(dist_mats) {
   dends = lapply(dist_mats, run_link, method = "single")
   real_dends = dends[lapply(dends, length) > 1]
